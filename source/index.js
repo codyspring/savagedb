9f349dfa58e459533b72cb3c6238be872d4f97c2
Cody Spring <codyspring@me.com>
2017-03-17 01:55:47 -0700

    Basic datastore with databases.
    Straight forward, give it a name and it returns that database. Don’t
    and it’ll send back the default. Creates one if none exist with that
    name.

---
+++ source/index.js
@@ -0,0 +1,20 @@
+const datastore = {}; // This is it.
+
+let defaultDb = 'default';
+
+function createDatabase(name) {
+  datastore[name] = Object.assign(
+    { data: {} }
+  );
+  return datastore[name];
+}
+
+createDatabase('default');
+
+const main = (name) => {
+  if (!name) return datastore[defaultDb];
+  if (datastore[name]) return datastore[name];
+  return createDatabase(name);
+};
+
+module.exports = main;
