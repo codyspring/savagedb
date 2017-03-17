763e9677146ba417bf1865cf52a2a20bf3f8e011
Cody Spring <codyspring@me.com>
2017-03-17 01:49:55 -0700

    Initial datastore tests

---
+++ test/index.js
@@ -0,0 +1,18 @@
+const expect = require('chai').expect;
+
+const database = require('../source/index');
+
+describe('database', function () {
+  it('should return a database object', function () {
+    const db = database();
+    expect(db).to.be.an.instanceof(Object);
+    expect(db).to.have.ownProperty('name');
+    expect(db).to.have.ownProperty('data');
+    expect(db.data).to.deep.equal({});
+  });
+
+  it('should return the right database when passed a name', function () {
+    const db = database('test');
+    expect(db.name).to.equal('test');
+  });
+});
