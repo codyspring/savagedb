f5641286abf5bd29141a314295b3864f524d92ba
Cody Spring <codyspring@me.com>
2017-03-17 02:00:58 -0700

    Tests for db collection.

--- test/index.js
+++ test/index.js
@@ -16,4 +16,22 @@ describe('database', function () {
       const newDb = database();
     });
   });
+
+  describe('#collection()', function () {
+    it('should throw an error when no collection is specified', function (done) {
+      const db = database();
+      try {
+        const collection = db.collection(); // eslint-disable-line no-unused-vars
+        done('no error thrown');
+      } catch (error) {
+        done();
+      }
+    });
+
+    it('should return a collection object, creating one if it doesn\'t exist ', function () {
+      const db = database();
+      const collection = db.collection('test');
+      expect(collection).to.be.an('object');
+    });
+  });
 });
