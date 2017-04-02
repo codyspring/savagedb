4764715c803daf95b3e1b785ce1c0fb001d65087
Cody Spring <codyspring@me.com>
2017-04-02 01:36:17 -0700

    checkpoint for persist options

--- source/database.js
+++ source/database.js
@@ -2,11 +2,26 @@ const store = require('./store');
 const events = require('./events');
 const persist = require('./persist');
 const Collection = require('./collection');
+const validate = require('aproba');
 
+// TODO: Validate
 function setPersistence(database, persistence) {
-  database.meta.persistence = 'yaml';
-  if (persistence === 'json') database.meta.persistence = persistence;
-  if (!persistence) database.meta.persistence = false;
+  if (!persistence || typeof persistence !== 'object') {
+    database.meta.persistence = false;
+    return;
+  }
+
+  switch (persistence.file) {
+    case 'yaml':
+      database.meta.persistence = 'yaml';
+      break;
+    case 'json':
+      database.meta.persistence = 'json';
+      break;
+    default:
+      database.meta.persistence = false;
+      break;
+  }
 }
 
 const create = (name, options = {}) => {
