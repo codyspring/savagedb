7de5320249d37be4182108d3db28dab877ecbb97
Cody Spring <codyspring@me.com>
2017-04-03 04:19:47 -0700

    call persist if it's supplied

--- source/database.js
+++ source/database.js
@@ -2,7 +2,7 @@ const store = require('./store');
 const Events = require('./events');
 const Collection = require('./collection');
 
-const create = (name, options) => {
+const create = (name, options = {}) => {
   store[name] = Object.assign(
     {
       name,
@@ -13,6 +13,8 @@ const create = (name, options) => {
   );
 
   store[name].subject('collection-created');
+
+  if (options.persist) options.persist(store[name]);
 
   return store[name];
 };
