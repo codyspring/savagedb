674c35d446e0b0ffb45b2d8da902f1b45a2741c0
Cody Spring <codyspring@me.com>
2017-04-02 20:20:59 -0700

    swapped events component
    It completely relies on being attached to a composed object. That
    object then has it’s own responsibility for creating and managing
    events.

--- source/events.js
+++ source/events.js
@@ -1,21 +1,12 @@
-const rx = require('rx');
-const EventEmitter = require('events').EventEmitter;
+const Rx = require('rx');
 
-const eventEmitter = new EventEmitter();
+module.exports = () => Object.assign({}, {
+  ownSubjects: {},
+  subject: function subject(name) {
+    if (!this.ownSubjects[name]) {
+      this.ownSubjects[name] = new Rx.Subject();
+    }
 
-// Dictionary of observables for events.
-const observables = {};
-
-// Creates an observable for an event.
-exports.observe = (event) => {
-  observables[event] = rx.Observable.fromEvent(eventEmitter, event);
-  return observables[event];
-};
-
-// Subscribes to an event type and returns the subscription.
-exports.subscribe = (event, callback) => observables[event].subscribe(callback);
-
-// Emit some event.
-exports.emit = (event, data) => {
-  eventEmitter.emit(event, data);
-};
+    return this.ownSubjects[name];
+  },
+});
