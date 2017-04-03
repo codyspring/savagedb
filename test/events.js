793b7c8376e407fa279b28a3b8f768e1c8278259
Cody Spring <codyspring@me.com>
2017-04-02 20:35:35 -0700

    better events tests

--- test/events.js
+++ test/events.js
@@ -5,13 +5,20 @@ const Events = require('../source/events');
 
 describe('::Events', function () {
   it('should return a composable events module', function () {
+    const mockDb = Object.assign( // eslint-disable-line no-unused-vars
+      {},
+      Events()
+    );
+  });
+
+  it('should contain a dictionary of subjects', function () {
     const mockDb = Object.assign(
       {},
       Events()
     );
 
-    expect(mockDb).to.have.ownProperty('subject');
-    expect(mockDb.subject).to.be.a.function;
+    expect(mockDb).to.have.ownProperty('ownSubjects');
+    expect(mockDb.ownSubjects).to.deep.equal({});
   });
 
   describe('#subject()', function () {
@@ -21,8 +28,29 @@ describe('::Events', function () {
         Events()
       );
 
-      const test = mockDb.subject('test');
-      expect(test).to.be.an.instanceOf(Rx.Subject);
+      const foo = mockDb.subject('foo');
+      expect(foo).to.be.an.instanceOf(Rx.Subject);
+    });
+
+    it('should add new Rx subjects to it\'s ownSubjects dictionary', function () {
+      const mockDb = Object.assign(
+        {},
+        Events()
+      );
+
+      const foo = mockDb.subject('foo');
+      expect(mockDb.ownSubjects.foo).to.equal(foo);
+    });
+
+    it('should return a current Rx subject if it exists', function () {
+      const mockDb = Object.assign(
+        {},
+        Events()
+      );
+
+      const foo = mockDb.subject('foo');
+      const bar = mockDb.subject('foo');
+      expect(foo).to.equal(bar);
     });
   });
 });
