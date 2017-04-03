4333c2aea1abdedd039b18aedaffd1291ae211e5
Cody Spring <codyspring@me.com>
2017-04-02 18:08:05 -0700

    strip persistence out of database

--- source/database.js
+++ source/database.js
@@ -1,36 +1,6 @@
 const store = require('./store');
 const events = require('./events');
-const persist = require('./persist');
 const Collection = require('./collection');
-<<<<<<< HEAD
-
-function setPersistence(database, persistence) {
-  database.meta.persistence = 'yaml';
-  if (persistence === 'json') database.meta.persistence = persistence;
-  if (!persistence) database.meta.persistence = false;
-=======
-const validate = require('aproba');
-
-// TODO: Validate
-function setPersistence(database, persistence) {
-  if (!persistence || typeof persistence !== 'object') {
-    database.meta.persistence = false;
-    return;
-  }
-
-  switch (persistence.file) {
-    case 'yaml':
-      database.meta.persistence = 'yaml';
-      break;
-    case 'json':
-      database.meta.persistence = 'json';
-      break;
-    default:
-      database.meta.persistence = false;
-      break;
-  }
->>>>>>> master
-}
 
 const create = (name, options = {}) => {
   store[name] = Object.assign(
@@ -40,11 +10,6 @@ const create = (name, options = {}) => {
     },
     Collection()
   );
-
-  setPersistence(store[name], options.persistence);
-  if (store[name].meta.persistence) {
-    persist.loadData(store[name]);
-  }
 
   events.emit('database-created', name);
   return store[name];
